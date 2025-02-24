5850a26fc620fa121e7158d725924f1f95c8a759
SayandipManna03 <academicsdm03@gmail.com>
2025-02-24 17:51:25 +0530

    first commit

---
+++ JAVASCRIPT/DAY 14/app.js
@@ -0,0 +1,7 @@
+// shortcut to clear console : ctrl + L
+// upward key to regain the previous command
+// a=5 
+// typeof a     Output= 'number'
+// let age=23; Variable Declaration
+// Comment out shortcut : Ctrl+/
+//         
